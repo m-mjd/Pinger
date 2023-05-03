a0870ee27ecac7ac770a2581b037d88c75c44a4f
mmdmmjd <120607317+mmdmmjd@users.noreply.github.com>
2023-05-03 09:35:48 -0700

    Add files via upload

---
+++ ping full/Data/start.bat
@@ -0,0 +1,9 @@
+attrib +h "Data\Starter.bat"
+color 3
+cls
+more Data\start.txt
+python Data\pingtest.py
+color 4
+CLS
+more Data/end.txt
+pause
---
+++ ping full/Starter.bat
@@ -0,0 +1,46 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install python --version=3.11.0 -y
+cls
+color 2 
+cls
+color 3
+cls
+pip install --upgrade pip
+cls
+pip install ping3
+cls
+@echo off
+move "Data\start.bat" ""
+move "pingtest.py" "Data"
+move "py.txt" "Data"
+move "start.txt" "Data"
+move "end.txt" "Data"
+attrib +h "Data"
+attrib -h "start.bat"
+attrib +h "Data\pingtest.py"
+attrib +h "Data\py.txt"
+attrib +h "Data\start.txt"
+attrib +h "Data\end.txt"
+cls
+more Data\py.txt
+pause
+move "Starter.bat" "Data"
